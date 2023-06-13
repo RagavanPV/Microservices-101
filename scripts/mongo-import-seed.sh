50d08bd470ada68d82d09f1b4522299a84cdb7fe
RagavanPV <ragavanpv007@gmail.com>
2023-06-13 13:01:35 +0530

    Updated search service to pull from Mongo and ES

--- scripts/mongo-import-seed.sh
+++ scripts/mongo-import-seed.sh
@@ -5,9 +5,10 @@ importDB="productsDB"
 collectionName="products"
 file_type="csv"
 folder_of_dataset="/Users/apple/Downloads/archive"
+port="27021"
 for csvfile in $folder_of_dataset/*.csv
 do
     echo "Importing $csvfile"
-    mongoimport --authenticationDatabase $authDB --username $username --password $password --db $importDB --collection $collectionName --type $file_type --headerline --ignoreBlanks --file $csvfile
+    mongoimport --authenticationDatabase $authDB --username $username --password $password --port $port --db $importDB --collection $collectionName --type $file_type --headerline --ignoreBlanks --file $csvfile
     echo "Import of $csvfile complete"
 done
--- search-service/README.md
+++ search-service/README.md
@@ -3,3 +3,7 @@
 Phase 1:
 Using Elastic Search as a search engine, because of its indexing capabilities
 First we start index on the dataset which are indexed in elastic search from mongodb
+
+Data from MongoDB are indexed to Elasticsearch using monstache
+
+monstache -f ./docker-compose/databases/products-service/config.toml -verbose
